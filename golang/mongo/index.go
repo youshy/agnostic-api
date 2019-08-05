ce7f35992a27dd44849d018df1a8be49542317c4
youshy <devilyoushy@gmail.com>
2019-08-05 16:48:10 +0100

    Fought my way here
    Implemented CRUD api in Golang

--- golang/mongo/index.go
+++ golang/mongo/index.go
@@ -6,8 +6,10 @@ import (
   "net/http"
   // "strconv"
   "encoding/json"
+  "strings"
 
   "github.com/gorilla/mux"
+  "github.com/ajg/form"
 
   mgo "gopkg.in/mgo.v2"
   "gopkg.in/mgo.v2/bson"
@@ -50,9 +52,111 @@ func getUsers(res http.ResponseWriter, req *http.Request) {
   json.NewEncoder(res).Encode(queryResult)
 }
 
+func getUser(res http.ResponseWriter, req *http.Request) {
+  queryParams := req.URL.Query()
+  // fnOk and lnOk are booleans for checking if the param is there
+  firstname, fnOk := queryParams["firstname"]
+  lastname, lnOk := queryParams["lastname"]
+
+  fmt.Println(firstname, lastname)
+
+  if fnOk && lnOk {
+    queryResult := User{}
+
+    collection := session.DB("mydb").C("users")
+
+    err := collection.Find(bson.M{"firstname": strings.Title(firstname[0]), "lastname": strings.Title(lastname[0])}).One(&queryResult)
+
+    if err != nil {
+      log.Print("Got error when reading: ", err)
+      return
+    }
+    json.NewEncoder(res).Encode(queryResult)
+  }
+}
+
+func postUser(res http.ResponseWriter, req *http.Request) {
+  var newUser User
+
+  decoded := form.NewDecoder(req.Body)
+  if err := decoded.Decode(&newUser); err != nil {
+    log.Print("Could not be decoded: ", err)
+    return
+  }
+
+  collection := session.DB("mydb").C("users")
+
+  err := collection.Insert(newUser)
+
+  if err != nil {
+    log.Print("Problem adding user to db", err)
+    return
+  }
+  log.Print("User added to the db")
+  fmt.Fprint(res, "User added to the db")
+}
+
+func updateUser(res http.ResponseWriter, req *http.Request) {
+  queryParams := req.URL.Query()
+
+  firstname, fnOk := queryParams["firstname"]
+  lastname, lnOk := queryParams["lastname"]
+
+  fmt.Println(firstname, lastname)
+
+  if fnOk && lnOk {
+    var editUser User
+
+    decoded := form.NewDecoder(req.Body)
+    if err := decoded.Decode(&editUser); err != nil {
+      log.Print("Could not be decoded: ", err)
+      return
+    }
+
+    collection := session.DB("mydb").C("users")
+
+    err := collection.Update(bson.M{"firstname": strings.Title(firstname[0]), "lastname": strings.Title(lastname[0])},
+            bson.M{"$set": bson.M{
+              "firstname":    editUser.Firstname,
+              "lastname":     editUser.Lastname,
+              "age":          editUser.Age,
+            }})
+    if err != nil {
+      log.Print("Problem updating user", err)
+      return
+    }
+    log.Print("Updated user")
+    fmt.Fprint(res, "Updated user")
+  }
+}
+
+func deleteUser(res http.ResponseWriter, req *http.Request) {
+  queryParams := req.URL.Query()
+
+  firstname, fnOk := queryParams["firstname"]
+  lastname, lnOk := queryParams["lastname"]
+
+  if fnOk && lnOk {
+    collection := session.DB("mydb").C("users")
+
+    err := collection.Remove(bson.M{"firstname": strings.Title(firstname[0]), "lastname": strings.Title(lastname[0])})
+    if err != nil {
+      log.Print("Problem removing user", err)
+      return
+    }
+    log.Print("User removed")
+    fmt.Fprint(res, "User removed")
+  }
+}
+
+
 func main() {
   router := mux.NewRouter()
   router.HandleFunc("/users", getUsers).Methods("GET")
+  router.HandleFunc("/user", getUser).Methods("GET")
+  router.HandleFunc("/user", postUser).Methods("POST")
+  router.HandleFunc("/user", updateUser).Methods("PUT")
+  router.HandleFunc("/user", deleteUser).Methods("DELETE")
   fmt.Println("Server is running on "+CONN_HOST+":"+CONN_PORT)
   defer session.Close()
   err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, router)
